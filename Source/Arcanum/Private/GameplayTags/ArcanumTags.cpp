eb2bb7bb842357394a64b0ee47c846fa18a628c3
BestSellerrr <rhkdals0921@daum.net>
2026-02-03 17:42:39 +0900

    기본 태그 설정

--- Source/Arcanum/Private/GameplayTags/ArcanumTags.cpp
+++ Source/Arcanum/Private/GameplayTags/ArcanumTags.cpp
@@ -3,4 +3,117 @@
 
 #include "GameplayTags/ArcanumTags.h"
 
+#pragma region KDH
+// 김도현
+// 설명
+// 플레이어 캐릭터가 아닌 
+// 플레이어가 가지고 있어야할 정보의 태그 모음
+namespace PlayerTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Player", "플레이어가 가지고 있어야할 정보의 모음");
+	namespace Currencies // 재화들
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Player.Currencies", "플레이어가 가지고 있는 재화들");
+		namespace Gold // 골드
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root,		"Player.Currencies.Gold",			"골드");
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value,		"Player.Currencies.Gold.Value",		"골드 현재값");
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(MaxValue,	"Player.Currencies.Gold.MaxValue",	"골드 최대값");
+		}
+		namespace Shard // 조각
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root,		"Player.Currencies.Shard",			"조각");
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value,		"Player.Currencies.Shard.Value",	"조각 현재값");
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(MaxValue,	"Player.Currencies.Shard.MaxValue", "조각 최대값");
+		}
+		namespace Soul // 소울
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root,		"Player.Currencies.Soul",			"소울"		);
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value,		"Player.Currencies.Soul.Value",		"소울 현재값"	);
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(MaxValue,	"Player.Currencies.Soul.MaxValue",	"소울 최대값"	);
+		}
+	}
 
+	namespace Inventories
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Player.Inventories", "현재 플레이어가 가지고 있는 것들");
+		namespace ConsumedItems // 사용 아이템들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.Inventories.ConsumedItems","사용 아이템들");
+		}
+
+		namespace Weapons // 무기들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.Inventories.Weapons",		"무기들");
+		}
+
+		namespace Equipments // 장비들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.Inventories.Equipments",	"장비들");
+		}
+
+		namespace Heroes // 영웅들(플레이어 캐릭터)
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.Inventories.Heroes",		"영웅들");
+		}
+
+		namespace Units // 아군 유닛들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.Inventories.Units",		"아군 유닛들");
+		}
+	}
+
+	namespace StageInfo // 클리어한 스테이지 정보
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.StageInfo", "클리어한 스테이지 정보") // 나중에 구조체에서는 배열같은 값을 불러올 것 같습니다
+	}
+
+	namespace LastEquips // 마지막으로 장착한 객체들
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Player.LastEquips", "마지막으로 장착한 객체들(게임시작시 불러옴)");
+		namespace Hero // 영웅(플레이어 캐릭터)
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.LastEquips.Hero",			"영웅(플레이어 캐릭터)");
+		}
+
+		namespace ConsumedItems // 사용 아이템들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.LastEquips.ConsumedItems","사용 아이테들");
+		}
+
+		namespace Equipments // 장비들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.LastEquips.Equipments",	"장비들");
+		}
+
+		namespace Weapons // 무기들
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Player.LastEquips.Weapons",		"무기들");
+		}
+	}
+}
+
+// 김도현
+// 설명
+// 플레이어 캐릭터들 계속 추가
+namespace PlayerCharacterTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "PlayerCharacters", "영웅(플레이어 캐릭터)들 태그, 이 태그로 해당 영웅의 정보를 가져올 수 있음");
+	namespace TestCharacter // KDH 테스트 캐릭터 태그입니다 구조체를 만들어 그것을 가져와야합니다
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "PlayerCharacters.TestCharacter", "TestCharacter라는 이름을 가진 영웅"); // 테스트 용
+	}
+}
+
+// 김도현
+// 설명
+// 무기들 계속 추가
+namespace Weapons
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Weapons", "무기들 태그, 이 태그로 해당 영웅의 정보를 가져올 수 있음");
+	namespace TestWeapon // KDH 테스트 무기 태그입니다 구조체를 만들어 그것을 가져와야합니다
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Value, "Weapons.TestWeapon", "TestWeapon이라는 이름을 가진 영웅"); // 테스트 용
+	}
+}
+#pragma endregion
--- Source/Arcanum/Public/GameplayTags/ArcanumTags.h
+++ Source/Arcanum/Public/GameplayTags/ArcanumTags.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
 
 /**
  * 
@@ -11,3 +12,119 @@ class ARCANUM_API ArcanumTags
 {
 
 };
+
+// 김도현
+// 설명
+// 플레이어 캐릭터가 아닌 
+// 플레이어가 가지고 있어야할 정보의 태그 모음
+namespace PlayerTags
+{
+	ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+	namespace Currencies // 재화들
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+		namespace Gold // 골드
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxValue);
+		}
+		namespace Shard // 조각
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxValue);
+		}
+		namespace Soul // 소울
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxValue);
+		}
+	}
+
+	namespace Inventories
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+		namespace ConsumedItems // 사용 아이템들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Weapons // 무기들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Equipments // 장비들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Heroes // 영웅들(플레이어 캐릭터)
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Units // 아군 유닛들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+	}
+
+	namespace StageInfo // 클리어한 스테이지 정보
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value) // 나중에 구조체에서는 배열같은 값을 불러올 것 같습니다
+	}
+
+	namespace LastEquips // 마지막으로 장착한 객체들
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+		namespace Hero // 영웅(플레이어 캐릭터)
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace ConsumedItems // 사용 아이템들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Equipments // 장비들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+
+		namespace Weapons // 무기들
+		{
+			ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+		}
+	}
+}
+
+// 김도현
+// 설명
+// 플레이어 캐릭터들 계속 추가
+namespace PlayerCharacterTags
+{
+	ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+	namespace TestCharacter // KDH 테스트 캐릭터 태그입니다 구조체를 만들어 그것을 가져와야합니다
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+	}
+}
+
+// 김도현
+// 설명
+// 무기들 계속 추가
+namespace Weapons
+{
+	ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root);
+	namespace TestWeapon // KDH 테스트 무기 태그입니다 구조체를 만들어 그것을 가져와야합니다
+	{
+		ARCANUM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Value);
+	}
+}
