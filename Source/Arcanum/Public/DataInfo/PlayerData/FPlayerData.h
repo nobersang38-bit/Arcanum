fdfc38105edab1caccf02fc8ceefd663b8610f55
1ter <cyh7845@gmail.com>
2026-04-06 03:33:32 +0900

    수정

--- Source/Arcanum/Public/DataInfo/PlayerData/FPlayerData.h
+++ Source/Arcanum/Public/DataInfo/PlayerData/FPlayerData.h
@@ -56,7 +56,7 @@ struct FPlayerData
     TMap<FGameplayTag, int32> StackCounts;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
-    int32 InventoryCapacity = 20;
+    int32 InventoryCapacity = 50;
 
     /* ==============================
      *         메일박스(캐릭터는 처음부터 다 주어지므로 별도 메일박스가 필요 없음)
@@ -65,7 +65,7 @@ struct FPlayerData
     TArray<FMailItem> Mailbox;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
-    int32 MailboxCapacity = 20;
+    int32 MailboxCapacity = 30;
 
     /* ==============================
      *     스테이지 클리어 정보?
